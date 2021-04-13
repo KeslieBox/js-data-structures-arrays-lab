60381ead1870d8a1d6f0e3c5a3887996fd472eec
KeslieBox <kesliebox@gmail.com>
2021-04-13 15:53:31 -0500

    commit tree 65f5384ed2e1c3c4326c863480333cc080c41e9a

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver (driver) {
+ console.log(drivers.push(driver));
+}
+
+function destructivelyPrependDriver (driver) {
+    console.log(drivers.unshift(driver));
+}
+
+function destructivelyRemoveLastDriver (driver) {
+console.log(drivers.pop());
+}
+
+function destructivelyRemoveFirstDriver (driver) {
+    console.log(drivers.shift());
+}
+
+function appendDriver (driver) {
+    return [...drivers, driver];
+}
+
+function prependDriver (driver) {
+    return [driver, ...drivers];
+}
+
+function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver () {
+    return drivers.slice(1);
+}
